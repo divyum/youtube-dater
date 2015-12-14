0231a869035175cde820b509ed5f8b26b78b4f33
divyum <divyum@browserstack.com>
2015-12-15 00:15:48 +0530

    [UPDATE]: publish date of show_more videos & run script on dom history change

--- README.md
+++ README.md
@@ -13,11 +13,11 @@ I made it coz I needed it. :wink:
 
 * For development:
 
-1. Clone this repo
-2. Open up Chrome and go to the extensions page (Window → Extensions)
-3. Enable developer mode (if it’s not already)
-4. Click on “Load unpacked extension”
-5. Select the folder for this app
+	1. Clone this repo
+	2. Open up Chrome and go to the extensions page (Window → Extensions)
+	3. Enable developer mode (if it’s not already)
+	4. Click on “Load unpacked extension”
+	5. Select the folder for this app
 
 ### Extra notes:
 
--- js/main.js
+++ js/main.js
@@ -27,21 +27,65 @@ function loadURL(url, pos, append_aft) {
   xhr.send();
 }
 
+function show_more_mutation() {
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if ( mutation.type == 'childList' ) {
+        if (mutation.addedNodes.length > 1) {
+          main('watch-more-related')
+        }
+      }
+    });
+  });
+  var show_more = document.getElementById('watch-more-related')
+  var observerConfig = {childList: true}
+  observer.observe(show_more, observerConfig);
+}
+
+function run_main_and_mutation(all_mutation) {
+  main('watch-related')
+  main('watch-sidebar-body')
+  show_more_mutation()
+  if(all_mutation)
+  {
+    watch_related_mutation()
+  }
+}
+
+function watch_related_mutation() {
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      for (var i = 0; i < mutation.addedNodes.length; i++) {
+        if(mutation.addedNodes[i].innerHTML != undefined)
+        {
+          if(mutation.addedNodes[i].innerHTML.indexOf("watch-related") > -1)
+          {
+            run_main_and_mutation(false)
+          }
+        }
+      }
+    })
+  })
+  var observerConfig = {childList: true, subtree: true, attributes: false, characterData: false}
+  observer.observe(document.getElementById('content'), observerConfig)
+}
+
 function main(tag) {
-  if(tag == 'watch-related')
+  if(tag == 'watch-related' || tag == 'watch-more-related')
   {
     sidebar_section = document.getElementById(tag)
+    len = document.querySelectorAll('#' + tag + '>li').length
   }
   else
   {
     sidebar_section = document.getElementsByClassName(tag)[0]
+    len = sidebar_section.children.length
   }
 
-  len = sidebar_section.children.length
   for(var i=0; i<len; i++)
   {
     var url, append_aft
-    if(tag == 'watch-related')
+    if(tag == 'watch-related' || tag == 'watch-more-related')
     {
       playlist = sidebar_section.children[i].className.indexOf('compact-video')
       if(playlist == -1)
@@ -59,7 +103,7 @@ function main(tag) {
       continue
     }
 
-    if(tag == 'watch-related')
+    if(tag == 'watch-related' || tag == 'watch-more-related')
     {
       url = sidebar_section.children[i].children[0].children[0].getAttribute('href')
       append_aft = sidebar_section.children[i].children[0].children[0]
@@ -75,19 +119,18 @@ function main(tag) {
 
 var prev = window.location.href
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.data.url != prev && request.data.url.indexOf('watch') > -1 ){
-      // console.log("URL CHANGED: " + request.data.url);
-      setTimeout(function(){ 
-        main('watch-related')
-        main('watch-sidebar-body')
-      }, 2000);
-      prev = window.location.href
-      console.log("new prev = " + prev)
-    }
-    sendResponse("gotcha");
+  var curr = request.data.url
+  if (request.data.url != prev && prev.indexOf('watch') == -1 && request.data.url.indexOf('watch') > -1 ){
+    console.log("URL CHANGED: " + request.data.url);
+    setTimeout(function(){
+      run_main_and_mutation(true)
+    }, 2000);
+    prev = curr
+    console.log("new prev = " + prev)
+  }
+  sendResponse("gotcha");
 });
 
 if(window.location.href.indexOf('watch') > -1){
-main('watch-related')
-main('watch-sidebar-body')
+run_main_and_mutation(true)
 }
